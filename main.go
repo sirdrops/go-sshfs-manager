01abb55f3df9159249e6072cd1e58209154bb77a
sirdrops <s.misiewicz92@gmail.com>
2024-04-26 13:37:24 +0200

    added opening,creating and reading file

--- main.go
+++ main.go
@@ -5,6 +5,35 @@ import (
 	"os"
 )
 
+var filepath string = "config.json"
+
+func checkFileCreateIfNotExist(path string) (*os.File, error) {
+	f, err := os.Open(path)
+
+	if err != nil {
+		f, err := os.Create(path)
+		if err != nil {
+			return f, err
+		}
+	}
+	return f, nil
+}
+
 func main() {
 
+	f, err := checkFileCreateIfNotExist(filepath)
+	if err != nil {
+		fmt.Println("Could not open or create file")
+	} else {
+		fmt.Println("File is ready to go")
+		defer f.Close()
+	}
+
+	b1 := make([]byte, 1000)
+	contents, err := f.Read(b1)
+	if err != nil {
+		fmt.Println("Cos zlego sie stanelo")
+	} else {
+		fmt.Printf("%s", string(b1[:contents]))
+	}
 }
